020e0aeb136582cdea73483625a39d2843ad3bb4
shilpashetty <shilpashetty0608@gmail.com>
2022-04-11 21:20:23 +0100

    commit message

---
+++ Candidate.java
@@ -0,0 +1,22 @@
+package com.learning.java;
+
+public class Candidate {
+        public static void main(String[]args){
+            int age=16;
+            if(age>16)
+
+                System.out.println("Eligible for voting");
+
+            else
+                System.out.println("cannot vote");
+        }
+
+
+
+    }
+
+
+
+
+
+
