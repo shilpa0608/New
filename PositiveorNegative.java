c23ee0ad4a34bda7ce638636a3ae83f549135458
shilpashetty <shilpashetty0608@gmail.com>
2022-04-11 22:48:15 +0100

    commit message

---
+++ PositiveorNegative.java
@@ -0,0 +1,14 @@
+package com.learning.java;
+
+public class PositiveorNegative {
+    public static void main(String[]args){
+        int x=2;
+        if(x>0){
+            System.out.println("number is positive");
+        }
+        if(x<0){
+            System.out.println("number is negative");
+        }
+
+    }
+}
